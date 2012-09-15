810915d71aa27a44e1832347357b9e388ead94e9
Cypress Frankenfeld <iamcypress@gmail.com>
2012-09-15 02:37:17 -0400

    broken: added a sleep model

--- morpheus/models.py
+++ morpheus/models.py
@@ -2,4 +2,14 @@ from django.db import models
 from datetime import datetime
 from django.contrib.auth.models import User
 
-class 
+# uncomment to create user profiles
+# class UserProfile(models.Model):
+#     # This field is required.
+#     user = models.OneToOneField(User)
+
+#     # Other fields here
+
+class Sleep(models.Model):
+    start = models.DateTimeField(unique=False)
+    end = models.DateTimeField(unique=False)
+    user = models.ForeignKey(User, unique=False)
